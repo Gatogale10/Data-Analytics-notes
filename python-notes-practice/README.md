27c14af867ab9f2c4ced93ad988872f6c755fdab
Gatogale10 <jorgegael235@gmail.com>
2026-06-02 16:47:44 -0600

    Bases-1

---
+++ python-notes-practice/README.md
@@ -0,0 +1,5 @@
+#En estas notas vendran explicadas las librerias y funciones de las mismas mas usadas en python para el analisis de datos. 
+
+Al igual se van a incluir ejercicios praticos de estadistica y de analisi de datos resueltos con estas herramientas - step by step.
+
+
---
+++ python-notes-practice/pandas_basics.py
@@ -0,0 +1,24 @@
+#Es importante importante la libreria de pandas como pd es un standar
+import pandas as pd
+
+#Despues se le tiene que asiganar un nombre a esta base de datos que ha sido importada
+#en este caso se le pone db pero podria ser cualquiera.
+db = pd.read_csv("datasets/tiktok_instagram_global_100countries.csv")
+
+#==============================COMANDOS PRINCIPALES=============================
+
+# Estos comandos son los que nos ayudan a hacer todo son como nuestros instrumentos de cocina
+# o nuestras herramientas como en cualquier teorema tenemos nuestras definiciones,lemas, etc..
+# tenemos nuestras propias cosas solo falta poneralas en el orden correcto. 
+# Bueno ademas de estas funciones tambien en nuestra caja de herramientas estan incluidos los
+# principos de programación, for, while, if todo lo que incluye un lenguaje de programacion ademas de 
+# otras librerias y todo lo que se pueda hacer con python, el fin resolver el problema, colocando las cosas 
+# en el orden correcto. 
+
+
+# Se podra todo lo que existe imaginablemente y logicamente para resolver un problema como lo planteamos 
+# logicamente en nuestra mente.Por ejemplo el copiar un dato, sacar promedios,minimos,maximos, tendencias
+# quitar datos, columnas, pegar bases, etc.....
+
+
+
