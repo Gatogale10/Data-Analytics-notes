3efa73174c1ca329e070c79707fd75868f85f047
Gatogale10 <jorgegael235@gmail.com>
2026-06-04 17:44:23 -0600

    Comandos iniciales r

--- r-notes-pratice/r-basics.r
+++ r-notes-pratice/r-basics.r
@@ -5,5 +5,25 @@
 # de datos completo,obvio estos son los comandos basicos y principales pero como en el caso 
 # de python tambien se subira un archivo con comandos para un nivel intermedio y avanzado.
 
-a = 1
+
+
+#Veamos un poco de los comandos basicos, en r se asignan los valores a una variable de 
+# forma distinta, en este caso con una flecha a la derecha
+a <- 2
+
+y<- "string"
+
+# En vez de poner = se coloca <- 
+
+#Todos los operadores, de suma, resta , multiplicación, división, etc...
+# son exactamente los mismos.
+
+
+#Aqui para crear vectores o arreglos a diferencias en Python o c usamos la funcion vector
+# en Python es a = [1,2,3] y en C como es costumbre tenemos que el tipo de variable
+# int a[] , es un tipo de dato arreglo con valores enteros, int a[] = {1,2,3,4}, nos dice los
+# datos que hay adentro. 
+
+
+
 
